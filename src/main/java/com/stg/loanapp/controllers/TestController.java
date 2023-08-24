df1990305b9cbc16f5f2e1f9f72bc88c282a87f2
Yuvashree Sukumar <yuvashrees@stg.com>
2023-08-24 16:19:03 +0530

    Updated an code to develop

--- src/main/java/com/stg/loanapp/controllers/TestController.java
+++ src/main/java/com/stg/loanapp/controllers/TestController.java
@@ -11,27 +11,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
 
-  @GetMapping("/user")
-  @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
-  public String userAccess() {
-    return "User Content.";
-  }
-   @GetMapping("/admin")
-  @PreAuthorize("hasRole('ADMIN')")
-  public String adminAccess() {
-    return "Admin Board.";
-  }
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User Content.";
+    }
 
- 
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin Board.";
+    }
 
-  @GetMapping("/mod")
-  @PreAuthorize("hasRole('MODERATOR')")
-  public String moderatorAccess() {
-    return "Moderator Board.";
-  }
- @GetMapping("/all")
-  public String allAccess() {
-    return "Public Content Modified.";
-  }
- 
+
+    @GetMapping("/mod")
+    @PreAuthorize("hasRole('MODERATOR')")
+    public String moderatorAccess() {
+        return "Moderator Board.";
+    }
+
+    @GetMapping("/all")
+    public String allAccess() {
+      
+        return "Public Content Modified.";
+    }
+
 }
